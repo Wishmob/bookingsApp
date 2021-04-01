84b3bc24e2143bc4bae51f6673fe17d63499a73d
Alex Breiter <alexander.breiter@stud.h-da.de>
2021-04-01 13:26:22 +0200

    initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/Wishmob/bookingsApp/pkg/config"
+	"github.com/Wishmob/bookingsApp/pkg/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"net/http"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
